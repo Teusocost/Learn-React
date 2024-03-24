862787d3bbda0aa7d3c8b0e1071ce2841a799142
Teusocost <mateus.costaar@gmail.com>
2024-03-24 21:29:20 +0000

    finish

--- aula_02/src/App.js
+++ aula_02/src/App.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "./component/Button";
 import { Link } from "react-router-dom";
 
-function App() {
-  return(
+class App extends Component {
 
-        <div >
-        <Button>
-        Salvar
-        </Button>
+    state = {
+        personagens: []
+    }
 
-        <Link to = "/personagem/1">
-            Personagem
-        </Link>
+    async componentDidMount() {
+        try {
 
-        </div>
-  );
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+            const dataJson = await response.json();
+
+            this.setState({ personagens: dataJson.results });
+            }
+            catch (error) {
+            console.log(error);
+            }
+    }
+
+    render(){
+        return(
+              <div >
+                {
+                    this.state.personagens.map(personagem => {
+                        return (
+                            <div key={personagem.name}>
+                                {personagem.name}
+                            </div>
+                        );
+                    })
+                }
+              </div>
+        );
+    }
 }
 
 export default App;
