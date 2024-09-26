fe8ae62fe1ecec8aece75a176d01d6e77f50741d
Mateus Costa <Teusocost>
2024-09-26 11:14:43 +0100

    Navbar-Container-Footer

--- ReactJS - Hora de codar/costs/src/App.js
+++ ReactJS - Hora de codar/costs/src/App.js
@@ -1,28 +1,29 @@
-import {BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Company from './components/pages/Company'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Container from './components/layout/Container'
+import Projects from './components/pages/Projects'
+
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
       <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">company</Link>
-        <Link to="/contact">contact</Link>
-        <Link to="/newproject">newproject</Link>
+        <Navbar />
       </div>
       <Container customClass = "min-height">
         <Routes>
           <Route exact path="/" element = {<Home />}/>
           <Route exact path="/company" element = {<Company />} />
           <Route exact path="/contact" element = {<Contact />} />
-          <Route exact path="/newproject" element = {<NewProject />} />
+          <Route exact path="/projects" element = {<Projects />} />
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      <Footer />
     </Router>
   );
 }
