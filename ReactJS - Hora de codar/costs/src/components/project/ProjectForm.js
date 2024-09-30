8ee90e7f73a1243974cd7d3771f6b3601e296ad1
Mateus Costa <Teusocost>
2024-10-01 00:12:05 +0100

    Conect_to_API

--- ReactJS - Hora de codar/costs/src/components/project/ProjectForm.js
+++ ReactJS - Hora de codar/costs/src/components/project/ProjectForm.js
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react'
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css';
 
 function ProjectForm({ btnText }) {
+
+
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    },[]);
+        
     return (
         <form className={styles.form}>
             <Input
@@ -18,7 +38,10 @@ function ProjectForm({ btnText }) {
                 placeholder="Insira o orcamento total"
                 name="budget"
             />
-            <Select name="category_id" text="Seleciona a categoria" />
+            <Select 
+                name="category_id" 
+                text="Seleciona a categoria" 
+                options = {categories}/>
             <SubmitButton text={btnText} />
         </form>
     )
