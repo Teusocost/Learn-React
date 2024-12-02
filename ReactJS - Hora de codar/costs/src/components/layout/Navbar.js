74e1bef05c45c6bc56945614097e81623db231d5
Mateus Costa <Teusocost>
2024-12-02 16:49:03 +0000

    test

--- ReactJS - Hora de codar/costs/src/components/layout/Navbar.js
+++ ReactJS - Hora de codar/costs/src/components/layout/Navbar.js
@@ -1,23 +1,36 @@
-import {Link} from 'react-router-dom'
-import Container from './Container'
-import styles from './Navbar.module.css'
-import logo from '../../img/costs_logo.png'
-
+import { Link } from "react-router-dom";
+import Container from "./Container";
+import styles from "./Navbar.module.css";
+import logo from "../../img/costs_logo.png";
 
 function Navbar() {
-    return (
-        <nav className={styles.navbar}>
-            <Container>
-                <Link to="/"><img src={logo} alt="costs"></img></Link>
-                <ul class={styles.list}>
-                    <li className={styles.item}> <Link to="/">Home</Link> </li>
-                    <li className={styles.item}> <Link to="/company">company</Link> </li>
-                    <li className={styles.item}> <Link to="/contact">contact</Link> </li>
-                    <li className={styles.item}> <Link to="/projects">Projects</Link> </li>
-                </ul>
-            </Container>
-        </nav>
-    )
+  return (
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="costs"></img>
+        </Link>
+        <ul class={styles.list}>
+          <li className={styles.item}>
+            {" "}
+            <Link to="/">Home</Link>{" "}
+          </li>
+          <li className={styles.item}>
+            {" "}
+            <Link to="/company">company</Link>{" "}
+          </li>
+          <li className={styles.item}>
+            {" "}
+            <Link to="/contact">contact</Link>{" "}
+          </li>
+          <li className={styles.item}>
+            {" "}
+            <Link to="/projects">Projects</Link>{" "}
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
